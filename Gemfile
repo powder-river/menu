4efaf50b8bf5ed64be8154c1a7de59f92905e86d
Danai Adkisson <dadkisso@live.com>
2015-06-08 21:30:42 -0400

    #shipit ready for heroku

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+ruby 2.1.1
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
@@ -23,6 +23,12 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+group :production do
+  gem 'rails_12factor'
+  gem 'pg'
+  gem 'puma'
+end
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -42,4 +48,3 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,34 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Course.create!(name: "Breakfast Waffles" )
-Course.create!(name: "International Waffles")
-Course.create!(name: "Legally Grey Waffles")
+Course.create!(name: "Appetizers" )
+Course.create!(name: "Entrees")
+Course.create!(name: "Specialty Beverages")
 
-Dish.create!(name: "Honey Waffle", description: "delicious", price: 2.50, course_id: 1)
-Dish.create!(name: "Bannana Foster Waffle ", description: "ok", price: 10.50, course_id: 1)
-Dish.create!(name: "Cookie Waffle ", description: "ok", price: 10.50, course_id: 1)
+Dish.create!(name: "Rocky Mountain Oysters", description: "A tantalizing fanfare from the Mountain
+west sure to please. No Bull!", price: 12.50, course_id: 1)
 
-Dish.create!(name: "Sweet and Sour Waffle", description: "awesome!", price: 12.50, course_id: 2)
-Dish.create!(name: "Vegimite Waffle", description: "delicious", price: 2.50, course_id: 2)
-Dish.create!(name: "Bannana Foster Waffle ", description: "ok", price: 10.50, course_id: 2)
+Dish.create!(name: "Pan Fried Alasken King Crab Cake", description: "Fresh Alasken King Crab prepared
+fresh bi-weekly. From the ocean strait to your mouth.", price: 20.50, course_id: 1)
+
+Dish.create!(name: "Nachos", description: "The working man's appetizer. Simple, Efficitve, yet
+still very much overpriced", price: 7.50, course_id: 1)
 
 
-Dish.create!(name: "Colorado Waffle", description: "Crave More!", price: 2.50, course_id: 3)
-Dish.create!(name: "Charlie Sheen Waffle", description: "ok", price: 10.50, course_id: 3)
-Dish.create!(name: "The Congressman Waffle", description: "ok", price: 10.50, course_id: 3)
+
+Dish.create!(name: "Overpriced French Dish", description: "An aromic blend of Canadian Mushroom,
+freh basil, bowtie pasta, and pheasant. A culinary standard or perhaps glorified hamburger helper, we'll
+let you be the judge", price: 50.99, course_id: 2)
+
+Dish.create!(name: "Bison Steak", description: "Prime graded bison, braised in whiskey and coke, served
+with country style mashed potatoes and corn on the cob.", price: 32.41, course_id: 2)
+
+Dish.create!(name: "The Gut Buster", description: "Some ask, is it better to have quality, or quantity?
+The answer is clearly quantity. This 5.5lb burger is an American classic. Can also be used for commercail
+livestock feed, or for feedind a 3rd world country" , price: 2.50, course_id: 2)
+
+
+Dish.create!(name: "Colorado Brownie", description: "Made fresh in house right here in the Caolinas!", price: 2.50, course_id: 3)
+Dish.create!(name: "H8erade", description: "dont h8", price: 10.50, course_id: 3)
+Dish.create!(name: "Figi Water", description: "Water Californians think is from figi, but like everything
+else there, is actually from Arizona. Who knew?", price: 10.50, course_id: 3)
